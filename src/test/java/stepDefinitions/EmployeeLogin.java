5f6368c01eb73b962d0f23664ae3f27c52cef718
unknown <amena.ibl@gmail.com>
2023-07-05 11:54:58 +0600

    feature added

---
+++ src/test/java/stepDefinitions/EmployeeLogin.java
@@ -0,0 +1,18 @@
+package stepDefinitions;
+
+import base.Base;
+import io.cucumber.java.en.Then;
+
+public class EmployeeLogin extends Base {
+
+    @Then("I click on Employee Login Menu")
+    public void i_click_on_employee_login_menu() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Verify I am in Employee Home Page")
+    public void verify_i_am_in_employee_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
